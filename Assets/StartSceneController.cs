e9c727101deb7d69eff4622e1a4eebf8df8b8b6f
Peter Nguyen <npeter389@gmail.com>
2024-03-08 03:11:23 -0800

    added settings scene and added audio slider bar

--- Assets/StartSceneController.cs
+++ Assets/StartSceneController.cs
@@ -7,12 +7,17 @@ using UnityEngine.SceneManagement;
 public class StartSceneController : MonoBehaviour
 {
     public string SampleScene;
+    public string SettingScene;
     public void LoadScene()
     {
         SceneManager.LoadScene(SampleScene);
     }
     public void Quit(){
         Application.Quit();
+    }
+
+    public void Settings(){
+        SceneManager.LoadScene(SettingScene);
     }
     // Start is called before the first frame update
    
