de0203e47a5748b3125ff9506ebe0d5950e169bd
Peter Nguyen <npeter389@gmail.com>
2024-03-13 16:09:46 -0700

    added limit to player hand in build deck scene
    faded message is bugged (does not always appear when limit is reached)

--- Assets/DropStorage.cs
+++ Assets/DropStorage.cs
@@ -2,22 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 
 public class DropStorage : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    
+    public Text messageText;
+    public float fadeDuration = .5f;
+
+    private Coroutine fadeOutCoroutine;
+
+
+    private IEnumerator FadeOutMessage()
+    {
+        float elapsedTime = 0f;
+        Color startColor = messageText.color;
+
+        // Fade out
+        while (elapsedTime < fadeDuration)
+        {
+            messageText.color = Color.Lerp(startColor, Color.clear, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // After fading, deactivate the message text
+        messageText.gameObject.SetActive(false);
+    }
     public void OnPointerEnter(PointerEventData eventData){
 
         
     }
 
-    public void OnDrop(PointerEventData eventData){
+    public void OnDrop(PointerEventData eventData)
+    {
         DragStorage d = eventData.pointerDrag.GetComponent<DragStorage>();
-        if (d!=null){
-            d.parentToReturnTo = this.transform;
-     
+        if (d != null)
+        {
+            Transform parentTransform = this.transform;
+
+            // Check if the number of children is less than 10 before allowing the drop
+            if (parentTransform.childCount < 10)
+            {
+                d.parentToReturnTo = parentTransform;
+            }
+            else
+            {
+                messageText.text = "Limit reached - cannot drop more items.";
+                messageText.gameObject.SetActive(true);
+                fadeOutCoroutine = StartCoroutine(FadeOutMessage());
+            }
         }
     }
+
 
     public void OnPointerExit(PointerEventData eventData){
         
@@ -27,7 +64,8 @@ public class DropStorage : MonoBehaviour, IDropHandler, IPointerEnterHandler, IP
     // Start is called before the first frame update
     void Start()
     {
-        
+        messageText.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
